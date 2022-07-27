44870753db464b2b1d5eeca3ed9d7a4f776e1514
LindsayLew <lmschwehr@gmail.com>
2022-07-27 09:47:54 -0700

    schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL(8,2),
-    department_id INT,
+    department_id INT NOT NULL,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE CASCADE
@@ -22,11 +22,9 @@ CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT,
-        FOREIGN KEY (role_id)
-        REFERENCES role(id),
+    role_id INT NOT NULL,
     manager_id INT,
-        FOREIGN KEY (manager_id)
-        REFERENCES employee(id)
+        FOREIGN KEY (role_id)
+        REFERENCES role(id)
 );
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,30 @@
 USE staff_db;
 
 INSERT INTO department (name)
-VALUES  (Sales),
-        (Finance),
-        (Marketing),
-        (Human Resources),
-        (IT);
+VALUES  ("Sales"),
+        ("Finance"),
+        ("Marketing"),
+        ("Human Resources"),
+        ("IT");
+INSERT INTO role (title, salary, department_id)
+VALUES  ("Sales Lead", 100000, 1),
+        ("Sales Associate", 60000, 1),
+        ("Payroll Clerk", 50000, 2),
+        ("Purchasing Manager", 100000, 2),
+        ("Marketing Analyst", 60000, 3),
+        ("Marketing Specialist", 70000, 3),
+        ("Recruiter", 65000, 4),
+        ("HR Coordinator", 50000, 4),
+        ("System Engineer", 100000, 5),
+        ("Support Specialist", 70000, 5);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ("Marcelyn", "Samuel", 1),
+        ("Sigmund", "Samaras", 3, 6),
+        ("Legend", "Laine", 10),
+        ("Mitra", "Pesty", 8),
+        ("Aurora", "Richard", 7),
+        ("Marshall", "Diaz", 4),
+        ("Yvain", "Grahame", 2,1),
+        ("Shavon", "Pander", 2,1),
+        ("Julianna", "Mills", 5);
+
